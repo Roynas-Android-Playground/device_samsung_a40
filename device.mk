ccb2e1222303d61442d35b0cc6024f325d097bde
LuK1337 <priv.luk@gmail.com>
2024-01-27 15:21:27 +0100

    a40: Switch to AIDL fingerprint HAL
    Change-Id: I588989bdc0ad8e21f0068664213899c10cb5b81d

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung
+    android.hardware.biometrics.fingerprint-service.samsung
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
