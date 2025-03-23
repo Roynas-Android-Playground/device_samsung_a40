e67fccec4bbfe042c9ed34d4e025b063fd7431c8
royna2544 <whiteshell2544@naver.com>
2025-03-23 20:15:18 +0900

    a40: Declare TARGET_SOC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5158993920
 BOARD_CACHEIMAGE_PARTITION_SIZE := 157286400
 BOARD_VENDORIMAGE_PARTITION_SIZE := 452984832
 
+# Target SoC
+TARGET_SOC := exynos7904
+
 # Inherit common board flags
 include device/samsung/exynos7885-common/BoardConfigCommon.mk
 
